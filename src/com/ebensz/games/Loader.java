efd76d6b5c3b92fbd009ce90b4df99d931c2ae12
luckxudong <luckxudong@gmail.com>
2012-02-04 06:49:39 +0000

    初始导入

---
+++ src/com/ebensz/games/Loader.java
@@ -0,0 +1,15 @@
+package com.ebensz.games;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Loader extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
