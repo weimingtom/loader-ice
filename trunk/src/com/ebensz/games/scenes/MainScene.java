1d37cba0e553da28d507228e4ee497d4a6aff6cc
luckxudong@gmail.com <luckxudong@gmail.com@af410ee3-9080-6866-c87b-e950bf604dab>
2012-02-04 07:36:10 +0000

    准备入口

--- trunk/src/com/ebensz/games/scenes/MainScene.java
+++ trunk/src/com/ebensz/games/scenes/MainScene.java
@@ -1,6 +1,9 @@
 package com.ebensz.games.scenes;
 
+import com.ebensz.games.R;
+import ice.engine.EngineContext;
 import ice.engine.Scene;
+import ice.node.widget.TextureGrid;
 
 /**
  * User: Jason
@@ -8,4 +11,14 @@ import ice.engine.Scene;
  * Time: 下午3:10
  */
 public class MainScene extends Scene {
+
+
+    public MainScene() {
+        int appWidth = EngineContext.getAppWidth();
+        int appHeight = EngineContext.getAppHeight();
+
+        //addChild(new TextureGrid(appWidth, appHeight, 0));
+    }
+
+
 }
