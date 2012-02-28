b6a8c61325f414da60d709692f05f8da3e2ead2e
luckxudong@gmail.com <luckxudong@gmail.com@af410ee3-9080-6866-c87b-e950bf604dab>
2012-02-28 14:23:45 +0000

    Game 调整

--- trunk/src/com/ebensz/games/Loader.java
+++ trunk/src/com/ebensz/games/Loader.java
@@ -1,10 +1,13 @@
 package com.ebensz.games;
 
-import com.ebensz.games.graphic.LoaderView;
+import android.opengl.GLU;
+import com.ebensz.games.graphic.LoaderRenderer;
 import com.ebensz.games.scene_providers.Loading;
 import ice.engine.Game;
 import ice.engine.GameView;
 import ice.engine.SceneProvider;
+import ice.graphic.GlRenderer;
+import ice.graphic.projection.PerspectiveProjection;
 
 public class Loader extends Game {
 
@@ -14,8 +17,14 @@ public class Loader extends Game {
     }
 
     @Override
-    protected GameView createRenderer() {
-        return new LoaderView(this);
+    protected GameView buildGameView() {
+
+        return new GameView(this) {
+            @Override
+            protected GlRenderer onCreateGlRenderer() {
+                return new LoaderRenderer();
+            }
+        };
     }
 
 }
