26f0589e9e6723a646fcbd6a42ca646fb5d10717
luckxudong@gmail.com <luckxudong@gmail.com@af410ee3-9080-6866-c87b-e950bf604dab>
2012-02-29 12:52:30 +0000

    准备加入悬浮事件

--- trunk/src/com/ebensz/games/Loader.java
+++ trunk/src/com/ebensz/games/Loader.java
@@ -1,13 +1,10 @@
 package com.ebensz.games;
 
-import android.opengl.GLU;
-import com.ebensz.games.graphic.LoaderRenderer;
+import com.ebensz.games.graphic.LoaderView;
 import com.ebensz.games.scene_providers.Loading;
 import ice.engine.Game;
 import ice.engine.GameView;
 import ice.engine.SceneProvider;
-import ice.graphic.GlRenderer;
-import ice.graphic.projection.PerspectiveProjection;
 
 public class Loader extends Game {
 
@@ -19,12 +16,7 @@ public class Loader extends Game {
     @Override
     protected GameView buildGameView() {
 
-        return new GameView(this) {
-            @Override
-            protected GlRenderer onCreateGlRenderer() {
-                return new LoaderRenderer();
-            }
-        };
+        return new LoaderView(this);
     }
 
 }
