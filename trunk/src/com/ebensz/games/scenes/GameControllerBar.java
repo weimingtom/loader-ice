0fc28d3a960977e2dc5c2f10c3c0126f422962dd
luckxudong@gmail.com <luckxudong@gmail.com@af410ee3-9080-6866-c87b-e950bf604dab>
2012-03-01 13:19:57 +0000

    控制栏

--- trunk/src/com/ebensz/games/scenes/GameControllerBar.java
+++ trunk/src/com/ebensz/games/scenes/GameControllerBar.java
@@ -2,6 +2,7 @@ package com.ebensz.games.scenes;
 
 import android.graphics.Bitmap;
 import com.ebensz.games.R;
+import com.ebensz.games.ui.widget.LimitNumberOverlay;
 import ice.node.OverlayParent;
 import ice.node.widget.BitmapOverlay;
 import ice.node.widget.ButtonOverlay;
@@ -19,22 +20,17 @@ public class GameControllerBar extends OverlayParent {
         BitmapOverlay controllerBg = new BitmapOverlay(Res.getBitmap(R.drawable.title));
         controllerBg.setPos(this.getPosX(), this.getPosY());
         addChild(controllerBg);
-        
+
         float componentHeight = this.getPosY() - 10;
 
         venue = new BitmapOverlay(Res.getBitmap(R.drawable.normal_venue));
-        venue.setPos(this.getPosX(),componentHeight);
+        venue.setPos(this.getPosX(), componentHeight);
         addChild(venue);
 
         Bitmap pointBgBitmap = Res.getBitmap(R.drawable.title_block_bg);
-        BitmapOverlay pointBg = new BitmapOverlay(100, pointBgBitmap.getHeight());
-        pointBg.setBitmap(R.drawable.title_block_bg);
-        pointBg.setPos(this.getPosX() - 306, componentHeight);
-        addChild(pointBg);
-
-        BitmapOverlay pointIconBitmap = new BitmapOverlay(Res.getBitmap(R.drawable.point_title));
-        pointIconBitmap.setPos(this.getPosX() - 336, componentHeight);
-        addChild(pointIconBitmap);
+        LimitNumberOverlay point = new LimitNumberOverlay(0, R.drawable.point_title);
+        point.setPos(this.getPosX() - 306, componentHeight);
+        addChild(point);
 
         BitmapOverlay bombBg = new BitmapOverlay(76, pointBgBitmap.getHeight());
         bombBg.setBitmap(R.drawable.title_block_bg);
@@ -45,18 +41,17 @@ public class GameControllerBar extends OverlayParent {
         bombIcon.setPos(this.getPosX() - 210, componentHeight);
         addChild(bombIcon);
 
-        serviceButton = new ButtonOverlay(R.drawable.service_title,R.drawable.service_title,R.drawable.service_title);
-        serviceButton.setPos(this.getPosX()+150,componentHeight);
+        serviceButton = new ButtonOverlay(R.drawable.service_title, R.drawable.service_title, R.drawable.service_title);
+        serviceButton.setPos(this.getPosX() + 150, componentHeight);
         addChild(serviceButton);
 
-        soundButton = new ButtonOverlay(R.drawable.sound_title,R.drawable.sound_title,R.drawable.sound_title);
-        soundButton.setPos(this.getPosX()+240,componentHeight);
+        soundButton = new ButtonOverlay(R.drawable.sound_title, R.drawable.sound_title, R.drawable.sound_title);
+        soundButton.setPos(this.getPosX() + 240, componentHeight);
         addChild(soundButton);
 
-        backButton = new ButtonOverlay(R.drawable.back_title,R.drawable.back_title,R.drawable.back_title);
-        backButton.setPos(this.getPosX()+330,componentHeight);
+        backButton = new ButtonOverlay(R.drawable.back_title, R.drawable.back_title, R.drawable.back_title);
+        backButton.setPos(this.getPosX() + 330, componentHeight);
         addChild(backButton);
-
     }
 
 
--- trunk/src/com/ebensz/games/ui/widget/LimitNumberOverlay.java
+++ trunk/src/com/ebensz/games/ui/widget/LimitNumberOverlay.java
@@ -5,25 +5,50 @@ import com.ebensz.games.R;
 import ice.node.OverlayParent;
 import ice.node.widget.BitmapOverlay;
 import ice.res.Res;
-import ice.util.BitmapUtils;
 
 /**
  * User: Mike.Hu
  * Date: 12-2-24
  * Time: 下午4:01
  */
-public class LimitNumberOverlay extends OverlayParent{
+public class LimitNumberOverlay extends OverlayParent {
 
+    public static final int BG_WIDTH = 75;
 
-    public LimitNumberOverlay(int width,int height) {
+    public LimitNumberOverlay(int number, int leftIconIndex) {
 
         super();
 
         Bitmap bgBitmap = Res.getBitmap(R.drawable.title_block_bg);
-       // bg = new BitmapOverlay(bgBitmap.getWidth(),);
+        bg = new BitmapOverlay(bgBitmap);
+        bg.setBounds(BG_WIDTH, bgBitmap.getHeight());
+        bg.setPos(this.getPosX(), this.getPosY());
+        addChild(bg);
+
+        leftIcon = new BitmapOverlay(leftIconIndex);
+        leftIcon.setPos(this.getPosX() - 20, this.getPosY());
+        addChild(leftIcon);
+
+        String numberString = String.valueOf(number);
+        int length = numberString.length();
+
+        rightNumber = new NumberOverlay(number);
+        leftIcon.setPos(this.getPosX() + 10, this.getPosY());
+        addChild(rightNumber);
+    }
+
+    public void setNumber(int number) {
+
+        Bitmap bgBitmap = Res.getBitmap(R.drawable.title_block_bg);
+        bg.setBounds(BG_WIDTH, bgBitmap.getHeight());
+    }
+
+    private Bitmap createMaxNumber() {
+
+        return null;
     }
 
     private BitmapOverlay bg;
     private BitmapOverlay leftIcon;
-    private BitmapOverlay rightNumber;
+    private NumberOverlay rightNumber;
 }
